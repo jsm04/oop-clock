61ddd0e7251da76905f9e040660c00c0dc6df1d3
jsm <jsmoretti.dev@outlook.com>
2025-07-08 20:26:08 -0300

    added update method

--- index.ts
+++ index.ts
@@ -54,6 +54,13 @@ class Clock extends EventEmitter {
 		}
 	}
 
+	update({ hour = 0, minute = 0, second = 0 }: ClockState ){
+		this.hour = hour % 24;
+		this.minute = minute % 60;
+		this.second = second % 60;
+		return this
+	}
+
 	stringifyState(): string {
 		const pad = (n: number) => n.toString().padStart(2, '0');
 		return `${pad(this.hour)}:${pad(this.minute)}:${pad(this.second)}`;
@@ -69,8 +76,14 @@ class Clock extends EventEmitter {
 
 const clock1 = new Clock({ hour: 12, minute: 45, second: 0 });
 const clock2 = new Clock({ hour: 15, minute: 25, second: 0 });
+const clock3 = new Clock({ hour: 9, minute: 1, second: 55 });
 
 console.log(clock1.compare(clock2));
 
 clock1.on('tick', console.log);
 clock2.on('tick', console.log);
+clock2.update({hour:2,minute:20, second:51})
+clock3.on('tick', console.log);
+
+
+console.log(clock2.compare(clock3));
